f9b113f29c20036bb2f86f76f500ef010f2e4075
jaqra <jaqra@hotmail.com>
2020-09-20 21:51:04 +0300

    Index added for User.Username & Default user added

--- src/Infrastructure/Data/ArchitectDbContext.cs
+++ src/Infrastructure/Data/ArchitectDbContext.cs
@@ -8,5 +8,12 @@ namespace Architect.Infrastructure.Data
         public DbSet<User> Users => Set<User>();
 
         public ArchitectDbContext(DbContextOptions<ArchitectDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasIndex(x => x.Username);
+
+            modelBuilder.Entity<User>().HasData(new User() { Id = 1, FirstName = "Admin", LastName = "Architect", Username = "admin", Password = "123456" });
+        }
     }
 }
