b1240904699ed5adaad03fc273cf6e4041e10b15
jaqra <jaqra@hotmail.com>
2020-10-17 23:08:59 +0300

    Implement IEntity

--- src/ApplicationCore/Models/User.cs
+++ src/ApplicationCore/Models/User.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Architect.ApplicationCore.Models
 {
-    public class User
+    public class User : IEntity
     {
         public int Id { get; set; }
 
